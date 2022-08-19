ad2658cee13d5d7260cc708c1348e98f8f3a1847
Marcus Maziero <marcus.maziero@paygo.com.br>
2022-08-19 09:55:56 -0300

    Create literal function

--- src/main/kotlin/com/study/kotlin/controller/MessageController.kt
+++ src/main/kotlin/com/study/kotlin/controller/MessageController.kt
@@ -1,28 +1,32 @@
 package com.study.kotlin.controller
 
 import com.study.kotlin.data.Message
+import com.study.kotlin.data.MessageTable
 import com.study.kotlin.service.MessageService
 import org.springframework.web.bind.annotation.*
 
 @RestController
 class MessageController(val service: MessageService) {
 
-    //:TODO precisa criar um literal function para essas conversões - acho que vai ficar melhor
     @GetMapping
     fun getMessages(): List<Message> {
         return service.findMessages().map {
-            Message(it.id, it.subject, it.text)
+            it.toMessage()
         }
     }
 
     @GetMapping(path = ["/{id}"])
     fun getMessageById(@PathVariable id: Int): Message {
         val teste = service.findMessage(id).orElseThrow() //TODO aqui precisa tratar corretamente
-        return Message(teste.id, teste.subject, teste.text)
+        return teste.toMessage()
     }
 
     @PostMapping
     fun postMessage(@RequestBody message: Message) {
         service.createMessage(message)
     }
+}
+
+fun MessageTable.toMessage(): Message {
+    return Message(this.id, this.subject, this.text)
 }
